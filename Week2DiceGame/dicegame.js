c99e7612aa0208da268028451f8e39345da6e73b
Brett Johnson <br.onson@outlook.com>
2019-04-23 15:13:26 -0500

    simplified operation with .charAt, added easter eggs

--- Week2DiceGame/dicegame.js
+++ Week2DiceGame/dicegame.js
@@ -1,6 +1,7 @@
 let roundArray = [4, 6, 8, 10, 20, 50];
-let lavaArray = ["0"];
+let lavaArray = [];
 let doubleArray = ["3", "5"];
+let easterEggArray = ["8", "11", "18", "39", "44"];
 let roundNumber = 0;
 let currentScore = 0;
 let highScore = 0;
@@ -30,7 +31,7 @@ function startGame(){
 		scoreRound(roundArray[roundNumber]);
 		break;
 		case 3:
-		alert("Rolling d10. All zeroes are now Lava! Is there any escape?");
+		alert("Rolling d10. All Zeroes are now Lava! Is there any escape?");
 		scoreRound(roundArray[roundNumber]);
 		break;
 		case 4:
@@ -64,30 +65,51 @@ function scoreRound(){
 	populateLavaArray();
 	let diceResult = diceRoll(roundArray[roundNumber]);
 	let resultString = diceResult.toString();
-	let resultArray = resultString.split("");
-	let judgedValue = (resultArray[resultArray.length - 1])
-	if (resultArray.includes("4")){
-		judgedValue = "4";
-	}
-	if (lavaArray.includes(judgedValue)){
-		alert("You rolled a " + diceResult +". You're in the Lava! You Lose! Better luck next time!");
+	if (easterEggArray.includes(resultString)){
+		switch(resultString.charAt(resultString.length - 1)){
+		case "1":
+		currentScore++;
+		currentScore = currentScore * 3;
+		alert("You rolled an " + diceResult + ". Lucky you, you found the Easter Egg! I had to code a whole new branch to say 'an " + diceResult + "' instead of 'a " + diceResult + "'. Triple points!! New Score is " + currentScore + ".");
+		break;
+		case "4":
+		alert("You rolled a " + diceResult + ". Really? How much more 'Four' can you get? You take a running start, then leap into the air and swan dive into the Lava with a stunning grace. It's breathtaking, really. I mean, you're still dead. But wow.");
 		roundNumber = 6;
+		break;
+		case "8":
+		alert("You rolled an " + diceResult + ". This is a bit of an Easter Egg, since I had to code a new branch to say 'an " + diceResult + "' instead of 'a " + diceResult + "'. Unfortunately, we both know Eights are Lava. So you still lose.");
+		roundNumber = 6;
+		break;
+		case "9":
+		alert("You've been building up speed as you've been deftly hopping over the lava from rock to rock, dodging rising pillars of smoke and cascading liquid rock. You're getting so close, the end is in sight. Then, like the Road Runner, you stop on a dime mere centimeters from the edge of a precipice and see the boiling hellscape stretching out before you. Fours. Fours, as far as the eye can see. You stumble back as an explosion rocks you from below, knocking you to the ground. So hot. So hopeless. It would be so easy to just give up, to just lay here and let the heat overtake you. Just then, at your darkest moment, you hear a faint sound rising over the din of the world falling apart. You sit up and cough, shielding your eyes from the dust and searching for the sound. Then you see the helicopter descending through the clouds, and it's heading your way. You jump to your feet and wave your arms, desperately trying to get the pilot's attention. When it nears, a ladder drops down and you grab on for dear life. The pilot calls down to you, but you can't hear her over the spinning blades and exploding rocks. So you climb. Hand over hand, rung over rung. Finally you get to cabin and collapse on the floor. With ragged breaths, you call out to the pilot, 'What did you say?'. She yells back, 'I said, you rolled a " + diceResult + ". Cutting it pretty close, aren't you? Triple Points!'");
+			currentScore++;
+			currentScore = currentScore * 3;
+		}
 	}
-	else if (doubleArray.includes(judgedValue)){
-		currentScore++;
-		currentScore += currentScore;
-		alert("You rolled a " + diceResult +", that was close! Your fancy footwork has narrowly avoided the lava! Double Points! New score is " + currentScore + ".");
+	else{
+		let judgedValue = resultString.charAt(resultString.length - 1);
+		if (resultString.includes("4")){
+			judgedValue = "4";
+		}
+		if (lavaArray.includes(judgedValue)){
+			alert("You rolled a " + diceResult + ". You're in the Lava! You Lose! Better luck next time!");
+			roundNumber = 6;
+		}
+		else if (doubleArray.includes(judgedValue)){
+			currentScore++;
+			currentScore += currentScore;
+			alert("You rolled a " + diceResult + ", that was close! Your fancy footwork has narrowly avoided the lava! Double Points! New score is " + currentScore + ".");
+		}
+		else {
+			currentScore++;
+			alert("You rolled a " + diceResult + ". You're safe for now! New Score is " + currentScore + ".");
+		}
 	}
-	else {
-		currentScore++;
-		alert("You rolled a " + diceResult +". You're safe for now! New Score is " + currentScore + ".");
-	}
-
 }
 
 function populateLavaArray(){
 	let increasedLavaFlow = roundArray[roundNumber].toString()
-	lavaArray.push(increasedLavaFlow);
+	lavaArray.push(increasedLavaFlow.charAt(increasedLavaFlow.length - 1));
 //		for (i = 0; i <= roundArray[roundNumber]; i++) {
 //		let lavaCheckerString = i.toString();
 //		let lavaCheckerArray = lavaCheckerString.split("");
