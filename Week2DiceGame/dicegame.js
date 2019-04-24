60d09a8ec5d55a0038b1fb8d2a017f2b1b065bec
Brett Johnson <br.onson@outlook.com>
2019-04-24 13:44:58 -0500

    project complete

--- Week2DiceGame/dicegame.js
+++ Week2DiceGame/dicegame.js
@@ -1,11 +1,10 @@
+let highScore = 0;
 let roundArray = [4, 6, 8, 10, 20, 50];
 let doubleArray = ["3", "5"];
 let easterEggArray = ["8", "11", "18", "39", "44"];
-let highScore = 0;
 let lavaArray = [];
 let roundNumber = 0;
 let currentScore = 0;
-
 
 let diceRoll = function(numberOfSides){
 	arrayOfSides = [];
@@ -16,38 +15,42 @@ let diceRoll = function(numberOfSides){
 	return result;
 }
 
-changeButtonText("START THE LAVA!");
+function beginGame(){
+	hideScore();
+	lavaBackground();
+	startRounds();
+	okButton();
+}
 
-function startGame(){
+function startRounds(){
 	while(roundNumber <= 7){
 		switch(roundNumber){
 		case 0:
 		changeText("Rolling d4. Careful, the Four is Lava!");
-		lavaBackground();
-		changeButtonText("OK");
-		rollRound()
+		rollRound();
 		return roundNumber; //Needed to break loop
 		case 1:
 		changeText("Rolling d6. Watch out! Sixes are now Lava, too!");
-		rollRound()
+		rollRound();
 		return roundNumber;
 		case 2:
 		changeText("Rolling d8. It's getting hot! Eights are now Lava!");
-		rollRound()
+		rollRound();
 		return roundNumber;
 		case 3:
 		changeText("Rolling d10. Zeroes are now Lava! Is there any escape?");
-		rollRound()
+		rollRound();
 		return roundNumber;
 		case 4:
 		changeText("Rolling d20. Where is this Lava coming from??");
-		rollRound()
+		rollRound();
 		return roundNumber;
 		case 5:
 		changeText("Rolling d50. OH GOD, SO MANY FOURS! LAVA IS EVERYWHERE!!");
-		rollRound()
+		rollRound();
 		return roundNumber;
 		case 6:
+		chopperBackground();
 		currentScore += 5;
 		changeText("You escaped the lava, great job! Bonus Points! Final Score is " + currentScore + ".");
 		roundNumber = 7;
@@ -73,10 +76,12 @@ function scoreRound(){
 		break;
 		case "4":
 		changeText("You rolled a " + diceResult + ". Really? How much more 'Four' can you get? You take a running start, then leap into the air and swan dive into the Lava with a stunning grace. It's breathtaking, really. I mean, you're still dead. But wow.");
+		anakinBackground();
 		roundNumber = 7;
 		break;
 		case "8":
 		changeText("You rolled an " + diceResult + ". This is a bit of an Easter Egg, since I had to code a new branch to say 'an " + diceResult + "' instead of 'a " + diceResult + "'. Unfortunately, we both know Eights are Lava. So you still lose.");
+		deadBackground();
 		roundNumber = 7;
 		break;
 		case "9":
@@ -94,6 +99,7 @@ function scoreRound(){
 		}
 		if (lavaArray.includes(judgedValue)){
 			changeText("You rolled a " + diceResult + ". You're in the Lava! You Lose! Better luck next time!");
+			deadBackground();
 			roundNumber = 7;
 		}
 		else if (doubleArray.includes(judgedValue)){
@@ -112,8 +118,8 @@ function scoreRound(){
 
 function rollRound(){
 		hideButton();
-		setTimeout(function(){scoreRound(roundArray[roundNumber])}, 1500);
-		setTimeout(function(){showButton()}, 1500);
+		setTimeout(function(){scoreRound(roundArray[roundNumber])}, 2000);
+		setTimeout(function(){showButton()}, 2000);
 }
 
 function populateLavaArray(){
@@ -122,11 +128,23 @@ function populateLavaArray(){
 }
 
 function lavaBackground(){
-	document.body.style.backgroundImage = "url('lava.jpg')";
+	document.body.style.backgroundImage = "url('images/lava.jpg')";
 }
 
 function normalBackground(){
-	document.body.style.backgroundImage = "url('start.png')";
+	document.body.style.backgroundImage = "url('images/start.png')";
+}
+
+function chopperBackground(){
+	document.body.style.backgroundImage = "url('images/chopper.jpg')";
+}
+
+function deadBackground(){
+	document.body.style.backgroundImage = "url('images/dead.jpg')";
+}
+
+function anakinBackground(){
+	document.body.style.backgroundImage = "url('images/anakin.jpg')";
 }
 
 function changeText(newText){
@@ -142,6 +160,16 @@ function hideButton(){
 function showButton(){
 	let buttonShower = document.getElementById("mainButton");
 	buttonShower.style.display = "inline-block";
+}
+
+function hideScore(){
+	let scoreHider = document.getElementById("scoreTracker");
+	scoreHider.style.display = "none";
+}
+
+function showScore(){
+	let scoreShower = document.getElementById("scoreTracker");
+	scoreShower.style.display = "inline-block";
 }
 
 function gameOver(){
@@ -164,8 +192,14 @@ function gameOverButton(){
 
 function resetButton(){
 	let buttonReseter = document.getElementById("mainButton");
-	buttonReseter.onclick = function() { startGame () };
+	buttonReseter.onclick = function() { beginGame() };
 	changeButtonText("START THE LAVA!");
+}
+
+function okButton(){
+	let buttonOker = document.getElementById("mainButton");
+	buttonOker.onclick = function() { startRounds() };
+	changeButtonText("OK");
 }
 
 function changeButtonText(newButtonText){
@@ -180,6 +214,5 @@ function resetGame(){
 	changeText("In this game, you will make a series of dice rolls. At the start of the game, the number 4 is Lava, and with each progessive roll, the Lava expands and takes over new numbers. Lava is bad, you should avoid it. But if you roll a number adjacent to a 4, you double your points. Can you make it to the end and escape the lava?");
 	normalBackground();
 	resetButton();
+	showScore();
 }
-
-// Next goal: transition alerts into inner html box
